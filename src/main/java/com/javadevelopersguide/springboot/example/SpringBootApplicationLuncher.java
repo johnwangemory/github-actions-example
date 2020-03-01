86531a071f88a98f7cbef462b410598da1bbcfa7
John Wang <john.wang@emory.edu>
2020-03-01 09:58:06 -0500

    JW updated java programs

---
+++ src/main/java/com/javadevelopersguide/springboot/example/SpringBootApplicationLuncher.java
@@ -0,0 +1,25 @@
+package com.javadevelopersguide.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+
+ * This Luncher for the spring boot application.
+
+ * 
+
+ * @author manoj.bardhan
+
+ *
+
+ */
+
+public class SpringBootApplicationLuncher {
+
+public static void main(String[] args) {
+
+SpringApplication.run(HelloWorldController.class, args);
+
+}
+
+}
